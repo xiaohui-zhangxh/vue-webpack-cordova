5f624ca8ace24e7b1f713f087bd7be9bb9b5ca5b
xiaohui <xiaohui@zhangxh.net>
2017-02-15 18:48:50 +0800

    fix errors

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,20 +1,6 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
-  </div>
+  <router-view></router-view>
 </template>
-
-<script>
-import Hello from './components/Hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-
-export default {
-  name: 'app',
-  components: {
-    Hello{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-</script>
 
 <style>
 #app {
